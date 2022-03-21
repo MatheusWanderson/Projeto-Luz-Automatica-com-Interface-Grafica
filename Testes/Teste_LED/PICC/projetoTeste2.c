d3327ae267a1342016a70c2babc65aeaad0dbccd
Wanderson Matheus da Silva <wandersonmatheus@outlook.com.br>
2022-03-20 22:13:20 -0300

    Alteracao_teste

---
+++ Testes/Teste_LED/PICC/projetoTeste2.c
@@ -0,0 +1,25 @@
+#INCLUDE <16f887.h>
+#USE DELAY (CLOCK=4000000)
+#FUSES XT,PROTECT,NOWDT,NOBROWNOUT,PUT,NOLVP
+#DEFINE SW PORTA, 1
+#DEFINE LED PORTB, 0
+#BYTE PORTA= 5
+#BYTE PORTB= 6
+MAIN() 
+{   
+     SET_TRIS_A(0B10);
+     SET_TRIS_B(0B11111110);     
+     WHILE(TRUE) 
+      {
+            IF(BIT_TEST(SW))
+            {
+                BIT_SET(LED);
+            }
+            ELSE      
+            {
+               BIT_CLEAR(LED);
+            }      
+      }
+}
+   
+   
